5dfec39e2d46d4e4b30e369253004b71f66bec37
FarajRiham <farajriham@gmail.com>
2024-12-13 10:59:45 +0100

    organisation

---
+++ Include/hashing.h
@@ -0,0 +1,8 @@
+//
+// Created by hp on 13/12/24.
+//
+
+#ifndef HASHING_H
+#define HASHING_H
+
+#endif //HASHING_H
---
+++ make_csv.cpp
@@ -0,0 +1,73 @@
+
+#include "cnpy/cnpy.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <random>
+#include <algorithm>
+
+struct Fingerprint {
+    int classLabel;
+    std::vector<float> data;
+};
+
+// Function to shuffle fingerprints
+void shuffleFingerprints(std::vector<Fingerprint>& fingerprints) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(fingerprints.begin(), fingerprints.end(), g);
+}
+int main() {
+
+    // Paths to .npy files
+    std::vector<std::string> filePaths = {
+        "/home/hp/make_csv/n01440764.npy",
+        "/home/hp/make_csv/n01443537.npy",
+        "/home/hp/make_csv/n01484850.npy",
+        "/home/hp/make_csv/n01491361.npy"
+    };
+
+    //container for all fingerprints that we will transform into the csv file
+    std::vector<Fingerprint> allFingerprints;
+
+
+    //Loop for each class to load the fingerprints
+    for(size_t classIdx = 0; classIdx < 4; ++classIdx) {
+        const std::string fileName = filePaths[classIdx];
+
+        //load  .npy file
+        cnpy::NpyArray arr = cnpy::npy_load(fileName);
+        float* rawData = arr.data<float>();
+        size_t numFingerprints = arr.shape[0];
+        size_t fingerprintSize = arr.shape[1];
+
+        //Limit to 1000 fingerprints or less
+        size_t numToSelect = std::min(numFingerprints, static_cast<size_t>(1000));
+
+        //Extract fingerprints and assign  class labels
+        for (size_t i = 0; i < numToSelect; ++i) {
+          std::vector<float> fingerprint(rawData + i * fingerprintSize, rawData + (i + 1) * fingerprintSize);
+          allFingerprints.push_back({static_cast<int>(classIdx + 1), fingerprint});
+        }
+
+    }
+
+    // Shuffle fingerprints
+    shuffleFingerprints(allFingerprints);
+
+    // Write to a CSV file
+    std::ofstream outFile("fingerprints_class.csv");
+    for (const auto& fp : allFingerprints) {
+        outFile << fp.classLabel << ",";
+        for (size_t i = 0; i < fp.data.size(); ++i) {
+            outFile << fp.data[i];
+            if (i < fp.data.size() - 1) {
+                outFile << ",";
+            }
+        }
+        outFile << "\n";
+    }
+    outFile.close();
+
+    return 0;
+}
