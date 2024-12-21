d63ba94c1703ce17a3c521ed1bfb002fa4b2bf2a
elkazdaz <zakaria.el-kazdam@grenoble-inp.org>
2024-12-21 11:48:04 +0100

    niveau 2 implementé mais avec redonandance et choix des collection à revoir

--- Include/DLSH_algo.h
+++ Include/DLSH_algo.h
@@ -10,30 +10,38 @@ class DLSH {
 public:
     /* Classe qui définit l'implémentation de l'algo :
     csvFilePath : Chemin vers le fichier CSV qui contient les fingerprints.
-    w : paramètre de l'objet choisit par l'utilisateur
-    L : un entier qui represente le nombre de table de hachage utilisé
+    w1 : paramètre de l'objet choisit par l'utilisateur
+    w2 : paramètre de l'objet choisit par l'utilisateur
+    L1 : un entier qui represente le nombre de table de hachage utilisé
+    L2 : un entier qui represente le nombre de table de hachage utilisé dans le niveau 2
     n : dimensions des embeddings
-    std::vector<HashingFunct> hashFunctions : l'ensemble des fct de hachage
+    std::vector<HashingFunct> hashFunctions1 : l'ensemble des fct de hachage dans le niveau 1
+    std::vector<HashingFunct> hashFunctions2 : l'ensemble des fct de hachage dans le niveau 1
+
     dataPoints : Données des fingerprints
 
  */
-    DLSH(const std::string& csvFilePath, int L, int n, int w);
+    DLSH(const std::string& csvFilePath, int L1, int L2,  int n, double w1, double w2);
 
     // Méthode pour exécuter l'algorithme DLSH au niveau 1
     std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double> >, VectorComparator<int> > computeHashTable_niv1();
 
     // Méthode pour exécuter l'algorithme DLSH au niveau 2
-    std::vector<std::vector<int>> computeHashTable_niv2(std::vector<std::vector<int>>& hashTable_niv1);
+    std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double> >, VectorComparator<int> > computeHashTable_niv2(
+            std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double> >, VectorComparator<int> >& hashTable_niv1);
 
     // Méthode pour charger les données depuis un fichier CSV
     void loadDataFromCSV();
 
 private:
     std::string csvFilePath;
-    int L;
+    int L1;
+    int L2;
     int n;
-    int w;
-    std::vector<HashingFunct> hashFunctions;
+    double w1;
+    double w2;
+    std::vector<HashingFunct> hashFunctions1;
+    std::vector<HashingFunct> hashFunctions2;
     std::vector<std::vector<double>> dataPoints;
 };
 
--- src/DLSH_algo.cpp
+++ src/DLSH_algo.cpp
@@ -5,10 +5,13 @@
 #include <unordered_map>
 
 // Constructeur
-DLSH::DLSH(const std::string& csvFilePath, int L, int n, int w)
-    : csvFilePath(csvFilePath), L(L), n(n), w(w) {
-    for (int i = 0; i < L; ++i) {
-        hashFunctions.push_back(generateLSHParameters(n, w));
+DLSH::DLSH(const std::string& csvFilePath, int L1, int L2,  int n, double w1, double w2)
+    : csvFilePath(csvFilePath), L1(L1), L2(L2), n(n), w1(w1), w2(w2) {
+    for (int i = 0; i < L1; i++) {
+        hashFunctions1.push_back(generateLSHParameters(n, w1));
+    }
+    for (int i = 0; i < L2; i++) {
+        hashFunctions2.push_back(generateLSHParameters(n, w2));
     }
 }
 
@@ -47,7 +50,38 @@ std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double
         throw std::runtime_error("Erreur : les données sont vides. Chargez les données depuis le fichier CSV.");
     }
 
-    return finalHash(dataPoints, hashFunctions, L, n);
+    return finalHash(dataPoints, hashFunctions1, L1, n);
+}
+
+std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double> >, VectorComparator<int> > DLSH::computeHashTable_niv2(
+        std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double> >, VectorComparator<int> >& hashTable_niv1){
+
+    std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double> >, VectorComparator<int> > finalResult;
+
+    for (const auto& [key, valueSet] : hashTable_niv1) {
+        // si le valueSet ne contient qu'un seul element , pas besoin de le decortiquer d'avance :)
+        if (valueSet.size() == 1 ){
+            // extract le seul point
+            std::vector<double> point = *valueSet.begin();
+            std::vector<int> cle (L2);
+
+            for( int j =0 ; j<L2 ; j++){
+                cle[j]= hashingComputing (point , hashFunctions2[j] );
+            }
+            finalResult[cle].insert(point);
+        }
+        else{
+            for (const auto& point : valueSet){
+                //pour chaque point , on calcule la nouvelle signature en utilisant les fonctions de hashage niveau 2
+                std::vector<int> cle(L2);
+                for( int j =0 ; j<L2 ; j++){
+                    cle[j]= hashingComputing (point , hashFunctions2[j]);
+                }
+                finalResult[cle].insert(point);
+            }
+        }
+    }
+    return finalResult;
 }
 
 
@@ -58,7 +92,8 @@ int main() {
         std::string csvFilePath = "/home/ensimag/3A/DLSH/Data/fingerprints_class.csv";
 
         // Paramètres pour l'algorithme DLSH
-        int L = 3;  // Nombre de fcts de hachage
+        int L1 = 3;  // Nombre de fcts de hachage niveau 1
+        int L2 = 6; // nombre de fonction de hachage niveau 2
         // Déduire la dimension des vecteurs (n) directement à partir du fichier CSV
         int n = 0; // Initialiser n avec 0 pour déterminer dynamiquement
 
@@ -80,20 +115,23 @@ int main() {
             --n; // Exclure la première colonne (le label)
         }
         file.close();
-        int w = 3;  // Largeur des bins
+        double w1 = 5;  // Largeur des bins
+        double w2 = 0.5 ; // largeur des bins mais pour niveau 2
 
         // Initialiser l'algorithme DLSH
-        DLSH dlsh(csvFilePath, L, n, w);
+        DLSH dlsh(csvFilePath, L1, L2, n, w1, w2);
 
         // Charger les données depuis le fichier CSV
         dlsh.loadDataFromCSV();
 
         // Calculer les tables de hachage niveau 1
         std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double> >, VectorComparator<int> > hashTable_niv1 = dlsh.computeHashTable_niv1();
+        std::map<std::vector<int>, std::set<std::vector<double>, VectorComparator<double> >, VectorComparator<int> > hashTable_niv2 = dlsh.computeHashTable_niv2(hashTable_niv1);
 
         // Afficher les résultats
         std::cout << "Résultats de la table de hachage (niveau 1) :\n";
         printDictionary(hashTable_niv1);
+
 
     } catch (const std::exception& e) {
         std::cerr << "Erreur : " << e.what() << std::endl;
